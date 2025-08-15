626e584be561c1af6ca85e8707eda78fded8457d
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-08-15 15:58:17 -0300

    fix congrats scene

--- Assets/SCRIPTS/Menu/ReturnToMenuRelay.cs
+++ Assets/SCRIPTS/Menu/ReturnToMenuRelay.cs
@@ -14,6 +14,8 @@ public class ReturnToMenuRelay : MonoBehaviour
         if (selfButton) selfButton.interactable = false;
 
         Time.timeScale = 1f; // por si estabas en pausa
+
+        Debug.Log("Relaymanda al menu");
         SceneManager.LoadScene(menuSceneName);
 
         //AudioManager.Instance.PlayMusicMenu();
